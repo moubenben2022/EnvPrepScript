3983ffe1e28369be275509ceb72db964a8c8a77a
Ming <ming@ming.com>
2024-01-11 13:31:39 -0800

    feature - add script for APIcenter env and start

---
+++ prepEnvApiCenter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This file should be stored at /home/production/apicenter
+
+LOG_PATH="/home/ming/Desktop/work/logs"
+# Set the LOG_PATH variable to the desired directory path.
+
+mkdir -p $LOG_PATH
+# Use the mkdir command with the -p option to create the directory if it doesn't exist.
+# The -p option ensures that parent directories are created as needed.
---
+++ startApiCenter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# this file should be stored at /home/production/apicenter
+docker pull mingming20200202/apicenter:latest
+
+docker tag docker.io/mingming20200202/apicenter:latest apicenter:latest
+
+docker stop apicenter
+
+docker rm apicenter
+
+# adjust to prts in APICenter/src/main/resources/application.yml
+# docker run -p host_port_1:container_port_1 -p host_port_2:container_port_2 -p host_port_3:container_port_3
+# docker run -v /path/on/host:/path/in/container image_name
+docker run --name apicenter -v "$LOG_PATH":"$LOG_PATH"  -p 12311:12311 -p 12312:12312 -d apicenter:latest
+
+docker image prune -
