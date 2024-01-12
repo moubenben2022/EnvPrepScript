a2021507817195d5353fcdd04bb1aa6f318f5b9f
Ming <ming@ming.com>
2024-01-12 14:45:58 -0800

    feature - add script for APIcenter env and start

--- prepEnvApiCenter.sh
+++ prepEnvApiCenter.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # This file should be stored at /home/production/apicenter
 
-LOG_DIR="/var/log"
-LOG_FILE="$LOG_DIR/APICenter.log"
+# LOG_DIR="/var/log"
+# LOG_FILE="$LOG_DIR/APICenter.log"
 
-# Create the directory if it doesn't exist
-mkdir -p "$LOG_DIR"
+# # Create the directory if it doesn't exist
+# mkdir -p "$LOG_DIR"
 
-# Create an empty file or do nothing if it already exists
-touch "$LOG_FILE"
+# # Create an empty file or do nothing if it already exists
+# touch "$LOG_FILE"
 
-# Grant read and write permissions to everybody
-chmod a+rw "$LOG_FILE"
+# # Grant read and write permissions to everybody
+# chmod a+rw "$LOG_FILE"
+
+# install maven
+sudo apt update
+sudo apt install maven
+mvn --version
